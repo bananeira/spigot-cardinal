72fd6d3daf122848eeb0a5059e5cd3aee18aa38a
bananeira <ijamil.osoria@gmail.com>
2022-05-29 17:58:14 +0200

    setup config and plugin instance

--- src/main/java/spigot/sage/cardinal/Cardinal.java
+++ src/main/java/spigot/sage/cardinal/Cardinal.java
@@ -1,17 +1,24 @@
 package spigot.sage.cardinal;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Cardinal extends JavaPlugin {
+    private static Cardinal plugin;
+    FileConfiguration config = getConfig();
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        plugin = this;
+        config.options().copyDefaults(true);
 
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    public static Cardinal getInstance() {
+        return plugin;
+    }
+
+    public FileConfiguration getConfigFile() {
+        return config;
     }
 }
